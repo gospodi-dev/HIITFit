1999487a721b5d970e28a78e337f330d671673c6
Gospodi <gospodijazzet@gmail.com>
2021-12-13 14:54:05 +0300

    Добавлен таймер интенсивных упражнений TimeInterval

--- HIITFit/Views/ExerciseView.swift
+++ HIITFit/Views/ExerciseView.swift
@@ -8,17 +8,18 @@ struct ExerciseView: View {
     let exerciseNames = ["Squat", "Step Up", "Burpee", "Sun Salute"]
     
     let index: Int
+    let interval: TimeInterval = 30
     
     var body: some View {
         GeometryReader { geometry in
             VStack {
-                HeaderView(exerciseNames: exerciseNames[index])
+                HeaderView(exerciseNames: exerciseNames[index]).padding(.bottom)
                 if let url = Bundle.main.url(forResource: videoNames[index], withExtension: "mp4") {
                     VideoPlayer(player: AVPlayer(url: url)).frame(height: geometry.size.height * 0.45)
                 } else {
                     Text("Couldn't find \(videoNames[index]).mp4").foregroundColor(.red)
                 }
-                Text("Timer")
+                Text(Date().addingTimeInterval(interval), style: .timer).font(.system(size: 90))
                 Text("Start/Done button")
                 Text("Rating")
                 Text("History button")

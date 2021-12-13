586663f3e91e2d10428c13a37ff952ffef4042ea
Gospodi <gospodijazzet@gmail.com>
2021-12-13 15:19:40 +0300

    Добавлена прибью для отображения рейтинга

---
+++ HIITFit/Views/RatingView.swift
@@ -0,0 +1,16 @@
+
+
+import SwiftUI
+
+struct RatingView: View {
+    var body: some View {
+        Image(systemName: "waveform.path.ecg")
+          .foregroundColor(.gray)
+    }
+}
+
+struct RatingView_Previews: PreviewProvider {
+    static var previews: some View {
+        RatingView()
+    }
+}

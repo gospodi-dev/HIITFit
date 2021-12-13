39ab7d0bb74bd7658288e190afef3f2d7092ae28
Gospodi <gospodijazzet@gmail.com>
2021-12-13 13:53:00 +0300

    Добавлен видео проигрыватель на страницу представление

--- HIITFit/Views/ExerciseView.swift
+++ HIITFit/Views/ExerciseView.swift
@@ -12,7 +12,11 @@ struct ExerciseView: View {
     var body: some View {
         VStack {
             HeaderView(exerciseNames: exerciseNames[index])
-            Text("Video player")
+            if let url = Bundle.main.url(forResource: videoNames[index], withExtension: "mp4") {
+                VideoPlayer(player: AVPlayer(url: url))
+            } else {
+                Text("Couldn't find \(videoNames[index]).mp4").foregroundColor(.red)
+            }
             Text("Timer")
             Text("Start/Done button")
             Text("Rating")

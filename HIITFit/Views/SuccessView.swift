f4bf4c11ffe9a8f7802015cb39b2a8f4440a1b3d
Gospodi <gospodijazzet@gmail.com>
2021-12-14 18:07:00 +0300

    добавлены текст и картинка экрана - Успех

--- HIITFit/Views/SuccessView.swift
+++ HIITFit/Views/SuccessView.swift
@@ -4,7 +4,21 @@ import SwiftUI
 
 struct SuccessView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image(systemName: "hand.raised.fill")
+                .resizedToFill(width: 75, height: 75)
+                .foregroundColor(.purple)
+            Text("High Five!")
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+            Text(
+                """
+                Отлично, Вы выполнили все четыре упражнения!
+                Помни, завтра будет другой, отличный день.
+                Так что ешьте хорошо и больше отдыхайте.
+                """
+            )
+        }
     }
 }
 

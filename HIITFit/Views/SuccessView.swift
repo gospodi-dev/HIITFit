22ac0e0568ac3bf09be3a8e6b1e348989bbdb448
Gospodi <gospodijazzet@gmail.com>
2021-12-14 18:14:40 +0300

    Добавлена кнопка Continue

--- HIITFit/Views/SuccessView.swift
+++ HIITFit/Views/SuccessView.swift
@@ -4,23 +4,30 @@ import SwiftUI
 
 struct SuccessView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "hand.raised.fill")
-                .resizedToFill(width: 75, height: 75)
-                .foregroundColor(.purple)
-            Text("High Five!")
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-            Text(
-                """
-                Отлично, Вы выполнили все четыре упражнения!
-                Помни, завтра будет другой, отличный день.
-                Так что ешьте хорошо и больше отдыхайте.
-                """
-            )
-                .multilineTextAlignment(.center)
-                .foregroundColor(.gray)
-            
+        
+        ZStack {
+            VStack {
+                Image(systemName: "hand.raised.fill")
+                    .resizedToFill(width: 75, height: 75)
+                    .foregroundColor(.purple)
+                Text("High Five!")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                Text(
+                    """
+                    Отлично, Вы выполнили все четыре упражнения!
+                    Помни, завтра будет другой, отличный день.
+                    Так что ешьте хорошо и больше отдыхайте.
+                    """
+                )
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.gray)
+            }
+            VStack {
+                Spacer()
+                Button("Continue") {}
+                .padding()
+            }
         }
     }
 }

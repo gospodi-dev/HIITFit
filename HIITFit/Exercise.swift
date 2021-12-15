1fe8481ffff0e6b53206abbb7b28c6ea52f2ff78
Gospodi <gospodijazzet@gmail.com>
2021-12-15 16:57:19 +0300

    Добавлена структура Exercise для формирования данных проекта для разработки

---
+++ HIITFit/Exercise.swift
@@ -0,0 +1,14 @@
+
+import Foundation
+
+struct Exercise {
+    let exerciseName: String
+    let videoName: String
+    
+    enum ExerciseEnum: String {
+        case squat = "Squat"
+        case stepUp = "Step Up"
+        case burpee = "Burpee"
+        case sunSalute = "Sun Salute"
+    }
+}

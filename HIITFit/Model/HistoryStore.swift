6b2c642cc9f332866c5219f9ca280c19fda58f4f
Gospodi <gospodijazzet@gmail.com>
2021-12-15 17:47:30 +0300

    Подготовлена модель для сохранения образцов данных истории просмотра клиентом

--- HIITFit/Model/HistoryStore.swift
+++ HIITFit/Model/HistoryStore.swift
@@ -1,4 +1,12 @@
 
 import Foundation
 
+struct ExerciseDay: Identifiable {
+    let id = UUID()
+    let date: Date
+    var exercises: [String] = []
+}
 
+struct HistoryStore {
+    var exerciseDay: [ExerciseDay] = []
+}

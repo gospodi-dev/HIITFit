7f5b2c8000a6ce5bf57da0f26798b551b17960bc
Gospodi <gospodijazzet@gmail.com>
2021-12-15 17:15:57 +0300

    инициализировали exercises массив к Exercise структуре

--- HIITFit/Exercise.swift
+++ HIITFit/Exercise.swift
@@ -12,3 +12,20 @@ struct Exercise {
         case sunSalute = "Sun Salute"
     }
 }
+
+extension Exercise {
+    static let exercises = [
+        Exercise(
+            exerciseName: ExerciseEnum.squat.rawValue,
+            videoName: "squat"),
+        Exercise(
+            exerciseName: ExerciseEnum.stepUp.rawValue,
+            videoName: "step-up"),
+        Exercise(
+            exerciseName: ExerciseEnum.burpee.rawValue,
+            videoName: "burpee"),
+        Exercise(
+            exerciseName: ExerciseEnum.sunSalute.rawValue,
+            videoName: "sun-salute")
+    ]
+}
